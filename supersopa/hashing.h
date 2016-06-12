59033eac6d23eb95e75c65b3c1a1ef4457cb0c04
Vylion <volfaria@gmail.com>
2016-06-12 09:59:28 +0200

    Hashing WIP #2. * Implemented naive next_prime() search. * Implemented a switch between 3 hashing methods. * hash_basic_sum: hash method summing the string characters and using next_prime(n) of received n for modulo. * hash_sum: hash method summing as if string were in "base K" where K is a prime and using received n for modulo. * hash_read: hash method that reads the string as a num and uses received n for modulo.

--- supersopa/hashing.h
+++ supersopa/hashing.h
@@ -10,6 +10,8 @@
 
 using namespace std;
 
+int next_prime(int n);
+int hash_basic_sum(string s, int n);
 int hash_sum(string s, int n);
 int hash_read(string s, int n);
 void by_bloom(Dictionary & dictionary, Board & board);
